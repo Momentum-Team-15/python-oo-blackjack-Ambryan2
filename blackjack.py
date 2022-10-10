c9dbde64d68493bd1594356d224b2fc10f1664c4
Your Name <alobryan2@gmail.com>
2022-10-10 18:31:41 -0400

    think classes are done all that is left is referring to them

--- blackjack.py
+++ blackjack.py
@@ -2,10 +2,17 @@ import random
 from tokenize import Number
 # Write your blackjack game here.
 class Game:
+
     def __init__ (self, deck, card, points):
         self.deck = deck
         self.card = card
-        # need to put rules in here
+        self.points = int(points)
+    
+    def remove_card(self, item):
+        if item in self.deck:
+            self.deck.remove(item)
+
+
 
 class Deck:
     def __init__ (self, suits, ranks):
@@ -14,13 +21,23 @@ class Deck:
         self.deck = []
         for suit in self.suits:
             for rank in self.ranks:
-                self.deck.append(f"{suit}{rank}")    
+                self.deck.append(f"{suit}{rank}") 
+
 
 class Card:
+    possible = ['A',2,3,4,5,6,7,8,9,10,'J','Q','K']
+
     def __init__(self, array):
         self.card = array[random.choice(range(0,len(array)))]
+        self.value = 0
         # need to assign value to each card
-        self.value = 10
+        for value in Card.possible:
+            if self.card[2] == 'A':
+                self.value = [1, 11] #have dealer ask question then it picks 
+            elif self.card[2] == 'J' or self.card[2] == 'Q' or self.card[2] == 'K':
+                self.value = 10
+            else:
+                self.value = self.card[2:len(self.card)]
 
 
 class Dealer:
@@ -40,23 +57,37 @@ class Player:
         Player.player_count += 1
 
 standard = Deck(['❤️', '♦️', '♠️', '♣️'],['A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K'])
-c = Card(standard.deck)
 d = Dealer('Yes')
-test = Game(standard.deck,c.card, 5)
-p1 = Player()
-p2 = Player()
 
-print(standard.deck)
+x = 0
+overallScore = 0
+while x < 2:
+    c = Card(standard.deck)
+    test = Game(standard.deck,c.card, c.value)
+    print(test.points)
+    overallScore += test.points
+    x += 1
+
+
+# p1 = Player()
+# p2 = Player()
+
+
+print(overallScore)
+# print(len(test.deck))
+# test.remove_card(c.card)
+# print(len(test.deck))
+# print(standard.deck)
 print('indiv card')
 print(c.card)
 print('card points')
 print(c.value)
-print('dealer choose hit')
-print(d.action)
-print('see if Game returns values from classes')
-print(test.card)
-print('see if player name is inputed')
-print(p1.name)
-print(p2.name)
-print(Player.player_count)
+# print('dealer choose hit')
+# print(d.action)
+# print('see if Game returns values from classes')
+# print(test.card)
+# print('see if player name is inputed')
+# print(p1.name)
+# print(p2.name)
+# print(Player.player_count)
 
