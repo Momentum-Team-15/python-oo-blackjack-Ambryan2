7bd37038dc6c534fe3d5719e587dd776a0798c0b
Your Name <alobryan2@gmail.com>
2022-10-10 13:52:35 -0400

    making progress and experimenting

--- blackjack.py
+++ blackjack.py
@@ -1,9 +1,33 @@
+import random
+from tokenize import Number
 # Write your blackjack game here.
-class Rules:
+class Game:
     pass
+
+class Deck:
+    def __init__ (self, suits, ranks):
+        self.suits = suits
+        self.ranks = ranks
+        self.deck = []
+        for suit in self.suits:
+            for rank in self.ranks:
+                self.deck.append(f"{suit}{rank}")    
+
+class Card:
+    def __init__(self, array):
+        self.card = array[random.choice(range(0,len(array)))]
+        self.value = 10
 
 class Dealer:
     pass
 
 class Player:
     pass
+
+t = Deck(['❤️', '♦️', '♠️', '♣️'],['A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K'])
+c = Card(t.deck)
+print(t.deck)
+print()
+print(c.card)
+print()
+print(c.value)
