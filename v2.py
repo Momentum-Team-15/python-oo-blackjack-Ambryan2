456670d93916f876df300196231f83ae029b6599
Your Name <alobryan2@gmail.com>
2022-10-11 10:38:42 -0400

    new improved blackjack

--- v2.py
+++ v2.py
@@ -1,41 +1,39 @@
 import random
 
 class Game:
-    # p_points = 0
-
     def __init__(self, deck, player):
         self.deck = deck
         self.card = deck[random.choice(range(0,len(deck)))]
         self.points = 0
-
         self.player = player
-        # self.p_points += self.points
+        self.hand = []
     
     # note need to draw card first when calling
     def draw_card(self, item, all_cards):
         self.card = all_cards[random.choice(range(0,len(all_cards)))]
-        if self.card[2] == 'A':
+        self.hand.append(self.card)
+        if self.card[3] == 'A':
             self.points = 0
-            answer = int(input(f"Do you want ace to be 1 or 11? "))
+            print(self.hand)
+            answer = int(input(f"{self.player} do you want ace to be 1 or 11? "))
             self.points = answer
-        elif self.card[2] == 'J' or self.card[2] == 'Q' or self.card[2] == 'K':
+        elif self.card[3] == 'J' or self.card[3] == 'Q' or self.card[3] == 'K':
             self.points = 0
             self.points = 10
         else:
             self.points = 0
-            self.points = self.card[2:len(self.card)]
+            self.points = self.card[3:len(self.card)]
         if item in self.deck:
             self.deck.remove(item)
         
 # makes deck
-suits = ['❤️', '♦️', '♠️', '♣️']
+suits = ['❤️ ', '♦️ ', '♠️ ', '♣️ ']
 ranks = ['A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K']
 deck = []
 for symbol in suits:
     for rank in ranks:
         deck.append(f"{symbol}{rank}")
-# card = deck[random.choice(range(0,len(deck)))]
-
+# Players involved
 player_1 = "Dealer"
 d_score = 0
 player_2 = "Player"
@@ -44,21 +42,58 @@ p_score = 0
 dealer = Game(deck, player_1)
 player = Game(deck, player_2)
 
+# give player two cards
+while len(dealer.hand) < 2 or len(dealer.hand) < 2:
+    dealer.draw_card(dealer.card, dealer.deck)
+    d_score += int(dealer.points)
+    player.draw_card(player.card, player.deck)
+    p_score += int(player.points)
+
+print(f"Dealer hand {dealer.hand}: Score = {d_score}")
+print(f"Player hand {player.hand} Score = {p_score}") 
+print(len(dealer.deck))
+
+if d_score < 17:
+    print("Dealer gets to play")
+    while d_score < 17:
+        dealer.draw_card(dealer.card, dealer.deck)
+        d_score += int(dealer.points)
+    print(f"Dealer hand {dealer.hand}: Score = {d_score}")
+
+answer = ''
+while answer != 'NO' and p_score < 21:
+    answer = input(f"{player.player} do you want to play (yes or no): ").upper()
+    if answer == 'YES':
+        player.draw_card(player.card, player.deck)
+        p_score += int(player.points)
+        print(f"Player hand {player.hand} Score = {p_score}") 
+
+if p_score > 21:
+    print('You lost because you busted!')
+elif p_score <= 21 and p_score > d_score:
+    print('You Won!!!')
+elif d_score > 21 and p_score < 21:
+    print('You Won!!')
+elif d_score == p_score:
+    print('Tie Game')
+else:
+    print('You lost')
+
 
 # this order prints the card and the points with card including ace
-dealer.draw_card(dealer.card, dealer.deck)
-print(dealer.card + "this is card wanted to add")
-print(len(dealer.deck))
-d_score += int(dealer.points)
-print(f"{d_score} this is wanted score from card")
-
-dealer.draw_card(dealer.card, dealer.deck)
-print(dealer.card + "This is card wanted to add")
-d_score += int(dealer.points)
-print(f"{d_score} this is wanted score")
 # dealer.draw_card(dealer.card, dealer.deck)
-print(len(dealer.deck))
+# print(f"{dealer.hand} card wanted to add")
+# print(len(dealer.deck))
+# d_score += int(dealer.points)
+# print(f"{d_score} this is wanted score from card")
 
-print(f"{len(player.deck)} this is length of player deck")
-# print(dealer.points)
-# print(dealer.player)
+# dealer.draw_card(dealer.card, dealer.deck)
+# print(dealer.card + "This is card wanted to add")
+# d_score += int(dealer.points)
+# print(f"{d_score} this is wanted score")
+# # dealer.draw_card(dealer.card, dealer.deck)
+# print(len(dealer.deck))
+
+# print(f"{len(player.deck)} this is length of player deck")
+
+
